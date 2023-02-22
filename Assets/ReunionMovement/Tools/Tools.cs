622364d389c903c6de587e2c93190958de282e24
长生 <64178985+JackeAstray@users.noreply.github.com>
2023-02-22 10:35:40 +0800

    更新工具脚本

--- Assets/ReunionMovement/Tools/Tools.cs
+++ Assets/ReunionMovement/Tools/Tools.cs
@@ -375,75 +375,6 @@ namespace GameLogic
             t.localRotation = Quaternion.identity;
             t.localScale = Vector3.one;
         }
-
-        /// <summary>
-        /// 计算中心点
-        /// </summary>
-        /// <param name="Points"></param>
-        /// <returns></returns>
-        public static Vector3 CalculateCenterPoint(List<Transform> Points)
-        {
-            Vector3 centerPoint = Vector3.zero;
-            foreach (Transform p in Points)
-            {
-                centerPoint += p.position;
-            }
-            centerPoint /= Points.Count;
-            return centerPoint;
-        }
-
-        /// <summary>
-        /// 计算AB与CD两条线段的交点.
-        /// </summary>
-        /// <param name="a">A点</param>
-        /// <param name="b">B点</param>
-        /// <param name="c">C点</param>
-        /// <param name="d">D点</param>
-        /// <param name="intersectPos">AB与CD的交点</param>
-        /// <returns>是否相交 true:相交 false:未相交</returns>
-        private static bool TryGetIntersectPoint(Vector3 a, Vector3 b, Vector3 c, Vector3 d, out Vector3 intersectPos)
-        {
-            intersectPos = Vector3.zero;
-
-            Vector3 ab = b - a;
-            Vector3 ca = a - c;
-            Vector3 cd = d - c;
-
-            Vector3 v1 = Vector3.Cross(ca, cd);
-
-            if (Mathf.Abs(Vector3.Dot(v1, ab)) > 1e-6)
-            {
-                // 不共面
-                return false;
-            }
-
-            if (Vector3.Cross(ab, cd).sqrMagnitude <= 1e-6)
-            {
-                // 平行
-                return false;
-            }
-
-            Vector3 ad = d - a;
-            Vector3 cb = b - c;
-            // 快速排斥
-            if (Mathf.Min(a.x, b.x) > Mathf.Max(c.x, d.x) || Mathf.Max(a.x, b.x) < Mathf.Min(c.x, d.x)
-               || Mathf.Min(a.y, b.y) > Mathf.Max(c.y, d.y) || Mathf.Max(a.y, b.y) < Mathf.Min(c.y, d.y)
-               || Mathf.Min(a.z, b.z) > Mathf.Max(c.z, d.z) || Mathf.Max(a.z, b.z) < Mathf.Min(c.z, d.z)
-            )
-                return false;
-
-            // 跨立试验
-            if (Vector3.Dot(Vector3.Cross(-ca, ab), Vector3.Cross(ab, ad)) > 0
-                && Vector3.Dot(Vector3.Cross(ca, cd), Vector3.Cross(cd, cb)) > 0)
-            {
-                Vector3 v2 = Vector3.Cross(cd, ab);
-                float ratio = Vector3.Dot(v1, v2) / v2.sqrMagnitude;
-                intersectPos = a + ab * ratio;
-                return true;
-            }
-
-            return false;
-        }
         #endregion
 
         #region 字符串
@@ -772,6 +703,46 @@ namespace GameLogic
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// 获取两点之间距离一定百分比的一个点
+        /// </summary>
+        /// <param name="start">起始点</param>
+        /// <param name="end">结束点</param>
+        /// <param name="distance">起始点到目标点距离百分比</param>
+        /// <returns></returns>
+        public static Vector3 GetBetweenPoint1(Vector3 start, Vector3 end, float percent)
+        {
+            Vector3 normal = (end - start).normalized;
+            float distance = Vector3.Distance(start, end);
+            return normal * (distance * percent) + start;
+        }
+
+        /// <summary>
+        /// 获取两点之间一定距离的点
+        /// </summary>
+        /// <param name="start">起始点</param>
+        /// <param name="end">结束点</param>
+        /// <param name="distance">距离</param>
+        /// <returns></returns>
+        public static Vector3 GetBetweenPoint2(Vector3 start, Vector3 end, float distance)
+        {
+            Vector3 normal = (end - start).normalized;
+            return normal * distance + start;
+        }
+
+        /// <summary>
+        /// 获取角度
+        /// </summary>
+        /// <param name="var1"></param>
+        /// <param name="var2"></param>
+        /// <returns></returns>
+        public static float GetAngel(Transform var1, Transform var2)
+        {
+            //注意角度测量一定要用对象的正方向
+            float angel = Vector3.Angle(var1.forward, var2.forward);
+            return angel;
         }
         #endregion
     }
