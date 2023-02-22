a7067802c5d554e83e26a63b19a31dccb6d7e24b
长生 <64178985+JackeAstray@users.noreply.github.com>
2023-02-22 23:42:41 +0800

    修复打开文件夹脚本，添加观察者模式基类

--- Assets/ReunionMovement/Editor/OpenPath.cs
+++ Assets/ReunionMovement/Editor/OpenPath.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,7 +12,7 @@ namespace GameLogic.Editor
         /// <summary>
         /// 打开 Data Path 文件夹。
         /// </summary>
-        [MenuItem("Game Framework/Open Folder/Data Path", false, 10)]
+        [MenuItem("工具箱/打开文件夹/Data Path", false, 10)]
         public static void OpenFolderDataPath()
         {
             Execute(Application.dataPath);
@@ -20,7 +21,7 @@ namespace GameLogic.Editor
         /// <summary>
         /// 打开 Persistent Data Path 文件夹。
         /// </summary>
-        [MenuItem("Game Framework/Open Folder/Persistent Data Path", false, 11)]
+        [MenuItem("工具箱/打开文件夹/Persistent Data Path", false, 11)]
         public static void OpenFolderPersistentDataPath()
         {
             Execute(Application.persistentDataPath);
@@ -29,7 +30,7 @@ namespace GameLogic.Editor
         /// <summary>
         /// 打开 Streaming Assets Path 文件夹。
         /// </summary>
-        [MenuItem("Game Framework/Open Folder/Streaming Assets Path", false, 12)]
+        [MenuItem("工具箱/打开文件夹/Streaming Assets Path", false, 12)]
         public static void OpenFolderStreamingAssetsPath()
         {
             Execute(Application.streamingAssetsPath);
@@ -38,7 +39,7 @@ namespace GameLogic.Editor
         /// <summary>
         /// 打开 Temporary Cache Path 文件夹。
         /// </summary>
-        [MenuItem("Game Framework/Open Folder/Temporary Cache Path", false, 13)]
+        [MenuItem("工具箱/打开文件夹/Temporary Cache Path", false, 13)]
         public static void OpenFolderTemporaryCachePath()
         {
             Execute(Application.temporaryCachePath);
@@ -49,7 +50,7 @@ namespace GameLogic.Editor
         /// <summary>
         /// 打开 Console Log Path 文件夹。
         /// </summary>
-        [MenuItem("Game Framework/Open Folder/Console Log Path", false, 14)]
+        [MenuItem("工具箱/打开文件夹/Console Log Path", false, 14)]
         public static void OpenFolderConsoleLogPath()
         {
             Execute(System.IO.Path.GetDirectoryName(Application.consoleLogPath));
@@ -63,18 +64,21 @@ namespace GameLogic.Editor
         /// <param name="folder">要打开的文件夹的路径。</param>
         public static void Execute(string folder)
         {
-            //folder = Utility.Text.Format("\"{0}\"", folder);
-            //switch (Application.platform)
-            //{
-            //    case RuntimePlatform.WindowsEditor:
-            //        Process.Start("Explorer.exe", folder.Replace('/', '\\'));
-            //        break;
-            //    case RuntimePlatform.OSXEditor:
-            //        Process.Start("open", folder);
-            //        break;
-            //    default:
-            //        throw new GameFrameworkException(Utility.Text.Format("Not support open folder on '{0}' platform.", Application.platform));
-            //}
+            if (string.IsNullOrEmpty(folder)) return;
+
+            folder = string.Format("\"{0}\"", folder);
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsEditor:
+                    Process.Start("Explorer.exe", folder.Replace('/', '\\'));
+                    break;
+                case RuntimePlatform.OSXEditor:
+                    Process.Start("open", folder);
+                    break;
+                default:
+                    UnityEngine.Debug.LogError(string.Format("不支持在“｛0｝”平台上打开文件夹。", Application.platform));
+                    break;
+            }
         }
     }
 }
