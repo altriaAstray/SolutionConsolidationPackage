60a4ed0f4e79eb9f0cae41f359cf8534ac7590ff
长生 <982109683@qq.com>
2023-03-19 21:45:22 +0800

    更新悬停

---
+++ Assets/ReunionMovement/Tools/HoverBase/Hover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLogic
+{
+    /// <summary>
+    /// 悬停
+    /// </summary>
+    public class Hover : HoverBase
+    {
+        
+    }
+}
---
+++ Assets/ReunionMovement/Tools/HoverBase/HoverBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace GameLogic
+{
+	/// <summary>
+    /// 悬停
+    /// </summary>
+    public class HoverBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+		public bool isHover;
+
+		public delegate void HoverHandler(bool hover);
+		public event HoverHandler GetIsHover;
+
+		private void Awake()
+		{
+			isHover = false;
+		}
+
+		/// <summary>
+		/// 进入悬停
+		/// </summary>
+		/// <param name="eventData"></param>
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			isHover = true;
+            GetIsHover(isHover);
+		}
+		/// <summary>
+		/// 离开悬停
+		/// </summary>
+		/// <param name="eventData"></param>
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			isHover = false;
+			GetIsHover(isHover);
+		}
+    }
+}
